919c9c51fbcc595aad5c8e2ce3772c6bb31d17d7
iomegak12 <jd.ramkumar@gmail.com>
2022-04-14 11:01:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PostviewerComponent } from './components/postviewer/postviewer.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { WherepipePipe } from './pipe/wherepipe.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostviewerComponent,
+    NavigationComponent,
+    WherepipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
